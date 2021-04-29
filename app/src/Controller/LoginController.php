dd9b7149f3734fd12310be2997376921c5a55c5e
Luca Santarella <luca.santarella@gmail.com>
2021-04-28 21:59:19 -0400

    JWT Claims changes

--- app/src/Controller/LoginController.php
+++ app/src/Controller/LoginController.php
@@ -8,6 +8,7 @@ use App\Database\Repository\UserRepository;
 use App\Request\LoginRequest;
 use DateTimeImmutable;
 use Lcobucci\JWT\Configuration as JwtConfiguration;
+use Psr\Http\Message\ResponseInterface;
 use Ramsey\Uuid\Uuid;
 use Spiral\Prototype\Traits\PrototypeTrait;
 
@@ -28,13 +29,13 @@ class LoginController
      * @param LoginRequest $login
      * @return array
      */
-    public function post(LoginRequest $login, UserRepository $userRepository, JwtConfiguration $jwtConfig): array
+    public function post(LoginRequest $login, UserRepository $userRepository, JwtConfiguration $jwtConfig): ResponseInterface
     {
         if (!$login->isValid()) {
-            return [
+            return $this->response->json([
                 'status' => 400,
                 'errors' => $login->getErrors()
-            ];
+            ]);
         }
 
         // application specific login logic
@@ -43,10 +44,10 @@ class LoginController
             $user === null
             || !password_verify($login->getField('password'), $user->password)
         ) {
-            return [
+            return $this->response->json([
                 'status' => 400,
                 'error' => 'No such user'
-            ];
+            ]);
         }
 
         // create token
@@ -55,21 +56,22 @@ class LoginController
         );
 
         // create id_token
-        $now   = new DateTimeImmutable();
-        $token = $jwtConfig->builder()
+        $now = new DateTimeImmutable();
+        $jwt = $jwtConfig->builder()
             ->issuedBy('https://cs490.lucasantarella.com')
             ->permittedFor('https://cs490.lucasantarella.com')
             ->identifiedBy(Uuid::uuid4())
             ->issuedAt($now)
             ->expiresAt($now->modify('+1 hour'))
             ->withClaim('uid', (string)$user->id)
+            ->withClaim('name', implode(' ', [$user->first_name, $user->last_name]))
+            ->withClaim('email', $user->email)
+            ->withClaim('un', $user->username)
             ->getToken($jwtConfig->signer(), $jwtConfig->signingKey());
-
-        return [
-            'status' => 200,
-            'message' => 'Authenticated!',
-            'id_token' => $token->toString()
-        ];
+        $this->cookies->set('jwt', $jwt->toString(), $now->modify('+1 hour')->getTimestamp(), null, null, null, false);
+        return $this->response
+            ->redirect('/dashboard', 302)
+            ->withAddedHeader('x-jwt', $jwt->toString());
     }
 
 }
